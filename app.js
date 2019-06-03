ad480df5eea1bca16b5e5c97914c08551ab77032
Tarik Medjber <tarikmedjber@Tariks-MacBook-Pro.local>
2019-06-03 14:20:05 +0100

    installed cors

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const apiRouter = require("./routes/api");
+const cors = require("cors");
 const { handle404, handle400, handle500 } = require("./errors");
 
 const app = express();
 
 app.use(express.json());
-
+app.use(corse());
 app.use("/api", apiRouter);
 
 app.use(handle400);
